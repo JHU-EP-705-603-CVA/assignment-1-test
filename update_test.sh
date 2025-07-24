57de295cda2247b824eaa81540116ab171c100de
Vince Pulido <j.vincepulido@gmail.com>
2025-07-24 18:00:12 -0400

    fixed update_test

--- update_test.sh
+++ update_test.sh
@@ -4,7 +4,10 @@ echo "Updating module..."
 git submodule update --remote --merge
 
 echo "Staging changes..."
-git add .
+git add ./test
+
+echo "Committing changes..."
+git commit -m "Updating testing modules"
 
 echo "Updating your repository..."
 git push
